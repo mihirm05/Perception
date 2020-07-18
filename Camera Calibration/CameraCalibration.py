236342e0fce2ae2c18b5e4edb042f5b52999e1a1
Mihir Mulye <mihirmulye95@gmail.com>
2020-07-18 09:12:21 +0200

    camera calibration for smartphone

---
+++ Camera Calibration/CameraCalibration.py
@@ -0,0 +1,111 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import glob
+
+np.set_printoptions(suppress=True)
+
+
+def calibration():
+    # Defining the dimensions of checkerboard
+    checkerboardPattern = (6, 9)
+
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.0001)
+    # print(criteria)
+
+    # Creating vector to store vectors of 3D points for each checkerboard image
+    objpoints = []
+    # Creating vector to store vectors of 2D points for each checkerboard image
+    imgpoints = []
+
+    # Defining the world coordinates for 3D points
+    objp = np.zeros((1, checkerboardPattern[0] * checkerboardPattern[1], 3), np.float32)
+    objp[0, :, :2] = np.mgrid[0:checkerboardPattern[0], 0:checkerboardPattern[1]].T.reshape(-1, 2)
+
+    # Extracting path of individual image stored in a given directory
+    path = os.getcwd() + '/images/'
+    images = glob.glob(path + 'c[0-9][0-9].jpg')
+
+    for fname in images:
+        plt.figure(figsize=(15, 15))
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Find the chess board corners
+        # If desired number of corners are found in the image then ret = true
+        ret, corners = cv2.findChessboardCorners(gray, checkerboardPattern,
+                                                 cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+
+        objpoints.append(objp)
+        # refining pixel coordinates for given 2d points.
+        corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, checkerboardPattern, corners, ret)
+        plt.xlabel('Image Width')
+        plt.ylabel('Image Height')
+        plt.imshow(img)
+        plt.title('Calibration')
+
+    cv2.destroyAllWindows()
+
+    # camera calibration
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    img = cv2.imread(path + 'calibration3.jpg')
+
+    plt.title('Distorted Image')
+    plt.show()
+    h, w = img.shape[:2]
+
+    # get new camera matrix
+    newCameraMatrix, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+    # un-distort the input image
+    undistortedImage = cv2.undistort(img, mtx, dist, None, newCameraMatrix)
+    plt.figure(figsize=(15, 15))
+    plt.imshow(undistortedImage)
+    plt.title('Undistorted Image')
+
+    print("Camera matrix (focal length in pixels) : \n")
+    print(newCameraMatrix)
+    print("distortion coefficients are: \n")
+    print(dist)
+
+    # conversion of focal length units (pixels to mm)
+    sensorDimensionWidth = 4.71
+    sensorDimensionHeight = 3.49
+    imageWidth = img.shape[0]
+    imageHeight = img.shape[1]
+
+    # formula for conversion adopted from OpenCV forum
+    newCameraMatrix[0][0] = newCameraMatrix[0][0] * sensorDimensionWidth / imageWidth
+    newCameraMatrix[1][1] = newCameraMatrix[1][1] * sensorDimensionHeight / imageHeight
+
+    print("Camera matrix (focal length in mm) : \n")
+    print(newCameraMatrix)
+
+    # projection error calculation (object points to image points)
+    mean_error = 0
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+        mean_error += error
+    print("total error: {}".format(mean_error / len(objpoints)))
+
+    # generating rotation matrix using rodrigues function
+    rotation_mat = np.zeros(shape=(3, 3))
+    R = cv2.Rodrigues(rvecs[0], rotation_mat)[0]
+    P = np.column_stack((np.matmul(mtx, R), tvecs[0]))
+    # print('R is: \n', R)
+    print('P matrix is: \n', P)
+
+
+def main():
+    calibration()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Camera Calibration/PMatrix.py
@@ -0,0 +1,125 @@
+import numpy as np
+
+
+def lineGenerator(pointOne, pointTwo):
+    """
+    :param pointOne: point A
+    :param pointTwo: point B
+    :return: coefficients of line between A and B in the form of
+    ax + b + c = 0
+    """
+
+    A = (pointOne[1] - pointTwo[1])
+    B = (pointTwo[0] - pointOne[0])
+    C = (pointOne[0] * pointTwo[1] - pointTwo[0] * pointOne[1])
+    return A/C, B/C, 1
+
+
+def intersectionGenerator(lineOne, lineTwo):
+    """
+    :param lineOne: equation of line one
+    :param lineTwo: equation of line two
+    :return: intersection point of two lines
+    """
+    x, y, z = np.cross(lineOne, lineTwo)
+    return int(x/y), int(y/z)
+
+
+def verifyPointLocation(point, line):
+    """
+    :param point: point coordinates
+    :param line: equation of line
+    :return: if point lies on line or not
+    """
+    point = np.asarray(point)[:, np.newaxis]
+    point = np.vstack((point, 1))
+    line = np.asarray(line)[:, np.newaxis]
+    valued = np.dot(point.T, line)
+
+    if valued < (1*e - 3):
+        print('Point lies on line')
+    else:
+        print('Point does not lie on the line')
+
+
+def distanceCalculator(pointOne, pointTwo):
+    """
+    :param pointOne: coordinates of first point
+    :param pointTwo: coordinates of second point
+    :return: distance between both points
+    """
+    return np.linalg.norm(pointOne - pointTwo)
+
+
+def acceptInput(string):
+    print(string)
+    return np.array([int(n) for n in input().split()])
+
+
+# values from PDF for reference:
+# b1 222 43
+# b2 389 168
+# t1 232 311
+# ~t1 344 340
+# t2 331 391
+# v 247 713
+# u' 590 451
+# u 767 451
+
+
+def main():
+    # b1 = acceptInput('Enter b1')
+    # b2 = acceptInput('Enter b2')
+
+    # v = acceptInput('Enter v')
+
+    # t1 = acceptInput('Enter t1')
+    # t2 = acceptInput('Enter t2')
+
+    b1 = np.array([[222, 43]])
+    b2 = np.array([[389, 168]])
+    v = np.array([[247, 713]])
+    t2 = np.array([[331, 391]])
+    t1 = np.array([232, 311])
+
+    b1b2 = lineGenerator(np.squeeze(b1), np.squeeze(b2))
+
+    # line assumed as the equation for the same is missing
+    l = lineGenerator([np.random.randint(1, 10), 451],
+                      [np.random.randint(21, 30), 451])
+    print(l)
+    # calculating the vanishing point coordinates
+    u = intersectionGenerator(b1b2, l)
+    print('vanishing point coordinates are: ', u)
+
+    # generating lines needed for calculating the ratio
+
+    b1v = lineGenerator(np.squeeze(b1), np.squeeze(v))
+    print('coefficients of b1v line: ', b1v)
+    b2v = lineGenerator(np.squeeze(b2), np.squeeze(v))
+    print('coefficients of b2v line: ', b2v)
+    t1u = lineGenerator(np.squeeze(t1), np.squeeze(u))
+    print('coefficients of b2v line: ', t1u)
+
+    tilde_t1 = intersectionGenerator(t1u, b2v)
+    print('~t1: ', tilde_t1)
+
+    # generating distances along given lines
+    vn = distanceCalculator(v, b2)
+    tilde_t1n = distanceCalculator(tilde_t1, b2)
+    t2n = distanceCalculator(t2, b2)
+
+    # formula adopted from the book
+    ratio = (tilde_t1n * (vn - t2n)) / (t2n * (vn - tilde_t1n))
+    print('Ratio of the segments ', ratio)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
